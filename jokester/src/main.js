f0f084033e6bfa1f907601588bacb4be648c3e9a
謝名凱 <xiemingkai@xiemingkaide-MacBook-Air.local>
2020-02-15 16:13:59 +0800

    MutatingTheStore(Jokester)

--- jokester/src/main.js
+++ jokester/src/main.js
@@ -3,6 +3,8 @@ import App from './components/App.vue'
 
 import store from './store'
 
+store.commit('INIT_JOKES', [{test: 'test_jokes'}, {test2: 'test_jokes2'}])
+
 new Vue({
     el: '#app',
     store,
